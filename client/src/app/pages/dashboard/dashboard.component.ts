8f2b6dc989069455ebc7e4b9bdcd5f159591f5ae
Wirokk <theo.gapihan@polymtl.ca>
2025-02-08 21:14:56 +0100

    Question component

--- client/src/app/pages/dashboard/dashboard.component.ts
+++ client/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { QuizPopupComponent } from '../../components/quiz-popup/quiz-popup.component';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [RouterModule],
+    imports: [RouterModule, QuizPopupComponent],
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent { }
